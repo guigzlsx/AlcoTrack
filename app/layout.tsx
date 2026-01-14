04d9beb53e00519fa66cef32b48daf4ff4db5620
Guillaume LE SAUX <glesaux@topcoach.local>
2026-01-14 09:45:20 +0100

    fix horizontal scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen overflow-hidden`}
       >
         {children}
       </body>
